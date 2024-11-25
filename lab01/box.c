f86c764de93588de8d026193df7a7e07bba1f6c9
Peeraphan Phuripattaraphan <phuripattaraphan7@gmail.com>
2024-11-25 16:53:34 +0700

    just 01

---
+++ lab01/box.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  double width,height,length;
+
+  printf("Enter width, length, and height: ");
+  scanf("%lf %lf %lf",&width,&height,&length);
+  printf("The volume of the box is %.2f",width*length*height);
+  
+
+
+  return 0;
+}
---
+++ lab01/lift.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int max_num;
+    int sum = 0;
+    int n,nub=0;
+    printf("Enter the maximum weight in kg: ");
+    do
+    {
+        printf("Enter passenger's weight in kg: ");
+        scanf("%d",&n);
+        sum += n;
+        nub += 1;
+    } while (sum < max_num);
+    if(max_num>sum){nub--;}
+    printf("The elevator can carry %d passenger(s)",nub);
+
+    }
